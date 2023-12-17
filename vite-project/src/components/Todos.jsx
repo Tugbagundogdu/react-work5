bc2325e91d66c60813ad19df1f4f9b5b3c7e1989
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-17 04:05:44 +0300

    patika todos work added

---
+++ vite-project/src/components/Todos.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+function Todos() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const toggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a task..."
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <label style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => toggleTask(index)}
+              />
+              {task.text}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
